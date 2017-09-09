0e4682a22f0dadf71912cc65484739c8c63f7a49
Michael Row <little93@me.com>
2017-09-09 18:50:20 +0800

    add: demo

---
+++ MusicPlayerDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MusicPlayerDemo
+//
+//  Created by Michael Row on 2017/9/9.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
