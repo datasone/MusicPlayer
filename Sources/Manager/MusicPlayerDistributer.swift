e0260195a611e2630d071335284b21b5c61c9257
Michael Row <little93@me.com>
2017-09-18 08:18:12 +0800

    add: MusicPlayerDistributer for timer event distribution

---
+++ Sources/Manager/MusicPlayerDistributer.swift
@@ -0,0 +1,16 @@
+//
+//  MusicPlayerDistributer.swift
+//  MusicPlayer
+//
+//  Created by Michael Row on 2017/9/18.
+//
+
+import Foundation
+
+class MusicPlayerDistributer {
+    
+    private var timer: Timer?
+    
+    
+    
+}
